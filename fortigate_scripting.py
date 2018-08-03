908b194ee029093fa3e11769bf6775342a3b6cc0
JL Cox <36089680+HoodieWhatie@users.noreply.github.com>
2018-08-03 15:20:02 -0500

    Update fortigate_scripting.py

--- fortigate_scripting.py
+++ fortigate_scripting.py
@@ -1,23 +1,36 @@
-import paramiko
-import sys
-import re
-
+#build SSH2 session
 def open_term(ip_add, user, pwd):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname=ip_add,username=user,password=pwd)
     
-    stdin, stdout, stderr=ssh.exec_command("show full-config")
+    #invoke shell and build channels (sockets) for IO features
+    channel = ssh.invoke_shell()
+    stdin = channel.makefile('wb')
+    stdout = channel.makefile('rb')
 
+    #execute code
+    stdin.write('''
+    config router setting
+    set hostname JLsLabRouter
+    get system status
+    ''')
+    
+    #read output
     result = stdout.readlines()
-    query = input('What are you looking for?:')
 
+    #check for error, print output if clean
     if result == []:
         error = ssh.stderr.readlines()
-        print >>sys.stderr, "ERROR: %s" % error
+        print('something went wrong...')
     else:
         for line in result:
             split_ln = line.split('\W')
             for bit in split_ln:
-                    if query in bit:
-                        print(bit)
+                print(bit)
+
+#executes code without requiring manual entry, testing purposes only
+for i in range(0,1):
+    print('executing open_term...')
+    open_term('10.0.0.220', 'admin', 'password')
+    print('program complete...')
